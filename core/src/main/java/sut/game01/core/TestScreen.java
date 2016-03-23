e5c9dbbb7458880922c31529c849b37ebe5c19fd
Anuchit Sawangchat <anuchit.sawangchat@hotmail.com>
2016-03-23 15:42:19 +0700

    Add Game

---
+++ core/src/main/java/sut/game01/core/TestScreen.java
@@ -0,0 +1,41 @@
+package sut.game01.core;
+
+import static playn.core.PlayN.*;
+import playn.core.Image;
+import playn.core.ImageLayer;
+import playn.core.Mouse;
+import playn.core.Touch;
+import tripleplay.game.Screen;
+import tripleplay.game.ScreenStack;
+
+public class TestScreen extends Screen {
+
+    private final ScreenStack ss;
+    private final ImageLayer bg;
+    private final ImageLayer backButton;
+
+    public TestScreen(final ScreenStack ss) {
+        this.ss = ss;
+
+        Image bgImage = assets().getImage("images/bg.png");
+        this.bg = graphics().createImageLayer(bgImage);
+
+        Image backImage = assets().getImage("images/back.png");
+        this.backButton = graphics().createImageLayer(backImage);
+        backButton.setTranslation(10,10);
+        backButton.addListener(new Mouse.LayerAdapter() {
+            @Override
+            public void onMouseUp(Mouse.ButtonEvent event) {
+                ss.remove(ss.top()); // <- pop screen
+            }
+        });
+
+    }
+
+    @Override
+    public  void  wasShown() {
+        super.wasShown();
+        this.layer.add(bg);
+        this.layer.add(backButton);
+    }
+}
