6d88cdd35b4fefedcc4a505f1664303e51b1ad5b
Anuchit Sawangchat <anuchit.sawangchat@hotmail.com>
2016-03-24 14:44:01 +0700

    new game#01

--- core/src/main/java/sut/game01/core/TestScreen.java
+++ core/src/main/java/sut/game01/core/TestScreen.java
@@ -17,16 +17,16 @@ public class TestScreen extends Screen {
     public TestScreen(final ScreenStack ss) {
         this.ss = ss;
 
-        Image bgImage = assets().getImage("images/bg.png");
+        Image bgImage = assets().getImage("images/gameBg.png");
         this.bg = graphics().createImageLayer(bgImage);
 
         Image backImage = assets().getImage("images/back.png");
         this.backButton = graphics().createImageLayer(backImage);
-        backButton.setTranslation(10,10);
+        backButton.setTranslation(530,10);
         backButton.addListener(new Mouse.LayerAdapter() {
             @Override
             public void onMouseUp(Mouse.ButtonEvent event) {
-                ss.remove(ss.top()); // <- pop screen
+                ss.remove(ss.top());
             }
         });
 
